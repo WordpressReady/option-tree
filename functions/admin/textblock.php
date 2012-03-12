be7728fd87b4bb48f76defd9e8cfb7e26525d98f
Fernando Zorrilla de SM <admin@wordpressready.com>
2012-03-12 13:42:29 -0300

    Added macro capability and shortcode processing.

--- functions/admin/textblock.php
+++ functions/admin/textblock.php
@@ -1,6 +1,8 @@
 <?php if (!defined('OT_VERSION')) exit('No direct script access allowed');
 /**
- * Text Block Option
+ * Text Block Option.
+ * 20120312: FZSM added macro capability and shortcode processing
+ * use #option_name# to get the option value.
  *
  * @access public
  * @since 1.0.0
@@ -18,7 +20,18 @@ function option_tree_textblock( $value, $settings, $int )
     <!-- <h3 class="text-title"><?php echo htmlspecialchars_decode( $value->item_title ); ?></h3> -->
     <div class="section">
       <div class="text_block">
-        <?php echo htmlspecialchars_decode( $value->item_desc ); ?>
+        <?php 
+  	
+		$theme_options=get_option('option_tree');
+		$macro_value= stripslashes( $value->item_desc );
+		
+		//loop options to replace option keys for values properly
+		foreach ($theme_options as $key => $value)
+		{
+			$macro_value=str_replace("#$key#","$value",$macro_value);
+		}
+		echo do_shortcode(htmlspecialchars_decode ($macro_value,ENT_QUOTES));
+		?>
       </div>
     </div>
   </div>
